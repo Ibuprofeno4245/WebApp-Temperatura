63da4f5c352720ed6e4c5385d9c8ab47d210e87d
TARCISIO NUNES <tarcnux@gmail.com>
2021-09-02 11:37:17 -0300

    clima

--- js/clima.js
+++ js/clima.js
@@ -6,22 +6,9 @@ function getClima() {
         url: 'https://api.openweathermap.org/data/2.5/weather?id=3468879&appid=231302b3ea3252b7e6c38e3ac5938fb2&lang=pt_br',
         dataType: 'json',
         success: function (data) {
-            console.log (data.weather[0].description);
-
-            
-            let converterCelcius = (data.main.temp - 273.15);
-            celsius = (Math.round(converterCelcius) + "C°")
-
-            $('#temperatura').html(celsius);
-            $('#condicao').html(data.weather[0].description);
-            $('#velocidade').html(data.wind.speed+'m/se');
-            $('#umidade').html(data.main.humidity+'%');
-            $('#dia').html(data.sys.sunrise);
-            $('#noite').html(data.sys.sunset);
-
-            let icone = 'img/' + data.weather[0].icon + '.png';
-            $('#i_condicao').attr('src',icone);
-
+            plotarResultados(data);
+            localStorage.clima = JSON.stringify(data);
+            localStorage.alteracaoCache = new Date().getTime();
         },
 
         error: function (argument) {
@@ -29,6 +16,24 @@ function getClima() {
 
         }
     });
+}
+
+function plotarResultados(data) {
+    console.log (data.weather[0].description);
+
+        let converterCelcius = (data.main.temp - 273.15);
+        celsius = (Math.round(converterCelcius) + "C°")
+
+        $('#temperatura').html(celsius);
+        $('#condicao').html(data.weather[0].description);
+        $('#velocidade').html(data.wind.speed+'m/s');
+        $('#umidade').html(data.main.humidity+'%');
+        $('#dia').html(data.sys.sunrise);
+        $('#noite').html(data.sys.sunset);
+
+        let icone = 'img/' + data.weather[0].icon + '.png';
+        $('#i_condicao').attr('src',icone);
+s
 }
 
 window.onload = function () {
