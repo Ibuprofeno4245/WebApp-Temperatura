066c011417d39e5ceb1cfb8f6818b1b9298b79e9
TARCISIO NUNES <tarcnux@gmail.com>
2021-09-02 08:50:36 -0300

    alguma coisa

---
+++ webapp-temperatura/webapp-temperatura/webapp-temperatura/js/clima.js
@@ -0,0 +1,37 @@
+function getClima() {
+
+    $.ajax({
+        method: 'get',
+        crossDomain: true,
+        url: 'http://api.openweathermap.org/data/2.5/weather?id=3468879&appid=231302b3ea3252b7e6c38e3ac5938fb2&lang=pt_br',
+        dataType: 'json',
+        success: function (data) {
+            console.log (data.weather[0].description);
+
+            let converterCelcius = (data.main.temp - 273.15);
+            celsius = (Math.round(converterCelcius) + "C°")
+
+            $('#temperatura').html(celsius);
+            $('#condicao').html(data.weather[0].description);
+            $('#velocidade').html(data.wind.speed+'km/h');
+            $('#umidade').html(data.main.humidity+'%');
+            $('#dia').html(data.sys.sunrise);
+            $('#noite').html(data.sys.sunset);
+
+            let icone = 'img/' + data.weather[0].icon + '.png';
+            $('#i_condicao').attr('src',icone);
+
+        },
+
+        error: function (argument) {
+            alert('Falha ao obter dados!');
+
+        }
+    });
+}
+
+window.onload = function () {
+
+    getClima();
+
+};
